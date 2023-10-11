4f7de9c1329cbb1b4327c3c3bb6b476af96851d3
Syed Husain <144152175+SyedAlamdarHusain@users.noreply.github.com>
2023-10-11 12:15:29 -0400

    Added Comments to App.py

--- SourceCode/Flask_App_Backend/Flask_React_Crud/App.py
+++ SourceCode/Flask_App_Backend/Flask_React_Crud/App.py
@@ -1,16 +1,24 @@
+# Import necessary libraries and modules
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
+
+# Create a Flask web application
 app = Flask(__name__)
+
+# Enable CORS for the app to allow cross-origin requests
 CORS(app)
+
+# Configure the SQLAlchemy database connection
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:turabi!123@localhost/studentreportsyedhusain'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db=SQLAlchemy(app)
+# Initialize SQLAlchemy and Marshmallow
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
-ma=Marshmallow(app)
-
+# Define the Students model
 class Students(db.Model):
     __tablename__ = 'students'
     StudentID = db.Column(db.Integer, primary_key=True)
@@ -22,63 +30,74 @@ class Students(db.Model):
         self.Name = Name
         self.CreditsEarned = CreditsEarned
 
+# Define the StudentSchema for serialization
 class StudentSchema(ma.Schema):
     class Meta:
         fields = ('StudentID', 'Name', 'CreditsEarned')
 
+# Initialize StudentSchema instances
 student_schema = StudentSchema()
 students_schema = StudentSchema(many=True)
 
+# Define a route for the home page
 @app.route("/")
 def hello_world():
     return "Hello World"
 
-@app.route('/listusers', methods =['GET'])
+# Route to list all students
+@app.route('/listusers', methods=['GET'])
 def listusers():
     all_students = Students.query.all()
     results = students_schema.dump(all_students)
     return jsonify(results)
 
-
+# Route to get details of a specific student
 @app.route('/userdetails/<int:StudentID>', methods=['GET'])
 def userdetails(StudentID):
     student = Students.query.get(StudentID)
     return student_schema.jsonify(student)
 
-@app.route('/userupdate/<int:StudentID>',methods = ['PUT'])
+# Route to update a student's information
+@app.route('/userupdate/<int:StudentID>', methods=['PUT'])
 def userupdate(StudentID):
     student = Students.query.get(StudentID)
 
+    # Extract data from the request JSON
     StudentID = request.json['StudentID']
     Name = request.json['Name']
     CreditsEarned = request.json['CreditsEarned']
+
+    # Update the student's information and commit to the database
     student.StudentID = StudentID
     student.Name = Name
     student.CreditsEarned = CreditsEarned
- 
     db.session.commit()
+
     return student_schema.jsonify(student)
 
- 
-@app.route('/userdelete/<int:StudentID>',methods=['DELETE'])
+# Route to delete a student
+@app.route('/userdelete/<int:StudentID>', methods=['DELETE'])
 def userdelete(StudentID):
     student = Students.query.get(StudentID)
     db.session.delete(student)
     db.session.commit()
     return student_schema.jsonify(student)
 
+# Route to add a new student
 @app.route('/useradd', methods=['POST'])
 def useradd():
     StudentID = request.json['StudentID']
     Name = request.json['Name']
     CreditsEarned = request.json['CreditsEarned']
 
+    # Create a new student and add to the database
     students = Students(StudentID=StudentID, Name=Name, CreditsEarned=CreditsEarned)
     db.session.add(students)
     db.session.commit()
 
     return student_schema.jsonify(students)
 
+# Define the Instructors model
 class Instructors(db.Model):
     __tablename__ = 'instructors'
     InstructorID = db.Column(db.Integer, primary_key=True)
@@ -90,31 +109,39 @@ class Instructors(db.Model):
         self.Name = Name
         self.Department = Department
 
+# Define the InstructorSchema for serialization
 class InstructorSchema(ma.Schema):
     class Meta:
         fields = ('InstructorID', 'Name', 'Department')
 
+# Initialize InstructorSchema instances
 instructor_schema = InstructorSchema()
 instructors_schema = InstructorSchema(many=True)
 
+# Route to list all instructors
 @app.route('/listinstructors', methods=['GET'])
 def listinstructors():
     all_instructors = Instructors.query.all()
     results = instructors_schema.dump(all_instructors)
     return jsonify(results)
 
+# Route to get details of a specific instructor
 @app.route('/instructordetails/<int:InstructorID>', methods=['GET'])
 def instructordetails(InstructorID):
     instructor = Instructors.query.get(InstructorID)
     return instructor_schema.jsonify(instructor)
 
+# Route to update an instructor's information
 @app.route('/instructorupdate/<int:InstructorID>', methods=['PUT'])
 def instructorupdate(InstructorID):
     instructor = Instructors.query.get(InstructorID)
 
+    # Extract data from the request JSON
     InstructorID = request.json['InstructorID']
     Name = request.json['Name']
     Department = request.json['Department']
+
+    # Update the instructor's information and commit to the database
     instructor.InstructorID = InstructorID
     instructor.Name = Name
     instructor.Department = Department
@@ -122,6 +149,7 @@ def instructorupdate(InstructorID):
     db.session.commit()
     return instructor_schema.jsonify(instructor)
 
+# Route to delete an instructor
 @app.route('/instructordelete/<int:InstructorID>', methods=['DELETE'])
 def instructordelete(InstructorID):
     instructor = Instructors.query.get(InstructorID)
@@ -129,12 +157,14 @@ def instructordelete(InstructorID):
     db.session.commit()
     return instructor_schema.jsonify(instructor)
 
+# Route to add a new instructor
 @app.route('/instructoradd', methods=['POST'])
 def instructoradd():
     InstructorID = request.json['InstructorID']
     Name = request.json['Name']
     Department = request.json['Department']
 
+    # Create a new instructor and add to the database
     instructor = Instructors(InstructorID=InstructorID, Name=Name, Department=Department)
     db.session.add(instructor)
     db.session.commit()
@@ -158,34 +188,34 @@ class CourseSchema(ma.Schema):
     class Meta:
         fields = ('CourseID', 'CourseTitle', 'InstructorID')
 
+# Initialize CourseSchema instances
 course_schema = CourseSchema()
 courses_schema = CourseSchema(many=True)
 
-# Add a route to list all courses
+# Route to list all courses
 @app.route('/listcourses', methods=['GET'])
 def listcourses():
     all_courses = Courses.query.all()
     results = courses_schema.dump(all_courses)
     return jsonify(results)
 
-# Add a route to get course details
+# Route to get course details
 @app.route('/coursedetails/<int:CourseID>', methods=['GET'])
 def coursedetails(CourseID):
     course = Courses.query.get(CourseID)
     return course_schema.jsonify(course)
 
-# Add a route to update course details
+# Route to update course details
 @app.route('/courseupdate/<int:CourseID>', methods=['PUT'])
 def courseupdate(CourseID):
     course = Courses.query.get(CourseID)
 
-    # Include CourseID in the request JSON and extract it
+    # Extract data from the request JSON
     CourseID = request.json['CourseID']
-
     CourseTitle = request.json['CourseTitle']
     InstructorID = request.json['InstructorID']
-    
-    # Set the updated values
+
+    # Update the course's information and commit to the database
     course.CourseID = CourseID
     course.CourseTitle = CourseTitle
     course.InstructorID = InstructorID
@@ -193,8 +223,7 @@ def courseupdate(CourseID):
     db.session.commit()
     return course_schema.jsonify(course)
 
-
-# Add a route to delete a course
+# Route to delete a course
 @app.route('/coursedelete/<int:CourseID>', methods=['DELETE'])
 def coursedelete(CourseID):
     course = Courses.query.get(CourseID)
@@ -202,20 +231,21 @@ def coursedelete(CourseID):
     db.session.commit()
     return course_schema.jsonify(course)
 
-# Add a route to add a new course
+# Route to add a new course
 @app.route('/courseadd', methods=['POST'])
 def courseadd():
-    CourseID = request.json['CourseID']  # Include CourseID in the request JSON
+    CourseID = request.json['CourseID']
     CourseTitle = request.json['CourseTitle']
     InstructorID = request.json['InstructorID']
 
+    # Create a new course and add to the database
     course = Courses(CourseID=CourseID, CourseTitle=CourseTitle, InstructorID=InstructorID)
     db.session.add(course)
     db.session.commit()
 
     return course_schema.jsonify(course)
 
-# Enrollments Model
+# Define the Enrollments model
 class Enrollments(db.Model):
     __tablename__ = 'enrollments'
     EnrollmentID = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -228,46 +258,53 @@ class Enrollments(db.Model):
         self.CourseID = CourseID
         self.Grade = Grade
 
-# Enrollment Schema
+# Define the EnrollmentSchema for serialization
 class EnrollmentSchema(ma.Schema):
     class Meta:
         fields = ('EnrollmentID', 'StudentID', 'CourseID', 'Grade')
 
+# Initialize EnrollmentSchema instances
 enrollment_schema = EnrollmentSchema()
 enrollments_schema = EnrollmentSchema(many=True)
 
-# Routes for Enrollments
+# Route to list all enrollments
 @app.route('/enrollments', methods=['GET'])
 def list_enrollments():
     all_enrollments = Enrollments.query.all()
     result = enrollments_schema.dump(all_enrollments)
     return jsonify(result)
 
+# Route to get enrollment details
 @app.route('/enrollmentdetails/<int:EnrollmentID>', methods=['GET'])
 def enrollment_details(EnrollmentID):
     enrollment = Enrollments.query.get(EnrollmentID)
     return enrollment_schema.jsonify(enrollment)
 
+# Route to add a new enrollment
 @app.route('/enrollmentadd', methods=['POST'])
 def enrollment_add():
     StudentID = request.json['StudentID']
     CourseID = request.json['CourseID']
     Grade = request.json['Grade']
 
+    # Create a new enrollment and add to the database
     enrollment = Enrollments(StudentID=StudentID, CourseID=CourseID, Grade=Grade)
     db.session.add(enrollment)
     db.session.commit()
 
     return enrollment_schema.jsonify(enrollment)
 
+# Route to update enrollment details
 @app.route('/enrollmentupdate/<int:EnrollmentID>', methods=['PUT'])
 def enrollment_update(EnrollmentID):
     enrollment = Enrollments.query.get(EnrollmentID)
 
+    # Extract data from the request JSON
     StudentID = request.json['StudentID']
     CourseID = request.json['CourseID']
     Grade = request.json['Grade']
 
+    # Update the enrollment details and commit to the database
     enrollment.StudentID = StudentID
     enrollment.CourseID = CourseID
     enrollment.Grade = Grade
@@ -275,6 +312,7 @@ def enrollment_update(EnrollmentID):
     db.session.commit()
     return enrollment_schema.jsonify(enrollment)
 
+# Route to delete an enrollment
 @app.route('/enrollmentdelete/<int:EnrollmentID>', methods=['DELETE'])
 def enrollment_delete(EnrollmentID):
     enrollment = Enrollments.query.get(EnrollmentID)
@@ -282,7 +320,7 @@ def enrollment_delete(EnrollmentID):
     db.session.commit()
     return enrollment_schema.jsonify(enrollment)
 
-
+# Start the Flask application
 if __name__ == '__main__':
     app.run(debug=True)
 
