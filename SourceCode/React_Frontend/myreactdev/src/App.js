3b65d5b1b1995aa9d9d8459b0d997b1d80bae6c8
Syed Husain <144152175+SyedAlamdarHusain@users.noreply.github.com>
2023-10-11 12:16:44 -0400

    Added comment to App.js

--- SourceCode/React_Frontend/myreactdev/src/App.js
+++ SourceCode/React_Frontend/myreactdev/src/App.js
@@ -1,6 +1,8 @@
+// Import React and necessary components from React Router
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+// Import page components for different routes
 import ListUserPage from "./pages/ListUserPage";
 import CreateUser from './pages/CreateUser';
 import EditUser from './pages/EditUser';
@@ -19,30 +21,37 @@ import EditEnrollment from './pages/EditEnrollment';
 
 function App() {
   return (
+    // Main application component
     <div className="vh-100 gradient-custom">
       <div className="container">
         <h1 className="page-header text-center">Student Report Card - Syed Husain </h1>
         <h1  className="page-header text-center">Class: CMSC 447</h1>
 
+        {/* Navigation buttons for different sections */}
         <button onClick={() => window.location.href = '/student'}>Student</button>
         <button onClick={() => window.location.href = '/instructor'}>Instructor</button>
         <button onClick={() => window.location.href = '/courses'}>Courses</button>
         <button onClick={() => window.location.href = '/enrollments'}>Enrollments</button>
 
+        {/* Define routes using BrowserRouter and Routes */}
         <BrowserRouter>
           <Routes>
+            {/* Student routes */}
             <Route path="/student" element={<ListUserPage />} />
             <Route path="/addnewuser" element={<CreateUser />} />
             <Route path="/student/user/:StudentID/edit" element={<EditUser />} />
 
+            {/* Instructor routes */}
             <Route path="/instructor" element={<ListInstructorPage />} />
             <Route path="/addnewinstructor" element={<CreateInstructor />} />
             <Route path="/instructor/instructor/:InstructorID/edit" element={<EditInstructor />} />
 
+            {/* Courses routes */}
             <Route path="/courses" element={<ListCoursePage />} />
             <Route path="/addnewcourse" element={<CreateCourse />} />
             <Route path="/course/:CourseID/edit" element={<EditCourse />} />
 
+            {/* Enrollments routes */}
             <Route path="/enrollments" element={<ListEnrollmentPage />} />
             <Route path="/addnewenrollment" element={<CreateEnrollment />} />
             <Route path="/enrollment/:EnrollmentID/edit" element={<EditEnrollment />} />
@@ -53,4 +62,6 @@ function App() {
   );
 }
 
+// Export the main App component
 export default App;
+
